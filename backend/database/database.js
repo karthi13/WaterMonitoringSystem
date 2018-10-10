747d15774748d57d282be3ada4025f2e6537c452
Techna987 <techna@windowslive.com>
2018-10-10 12:45:15 +0200

    Authentication

---
+++ backend/database/database.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+var connection = mysql.createPool({
+    connectionLimit: 100,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'water',
+    port: 3306,
+    debug: false,
+    multipleStatements: true
+});
+module.exports.connection = connection;
+
