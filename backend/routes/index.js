6317413a1036ced8391cfe0e8c0bd75a157d3713
Techna987 <techna@windowslive.com>
2018-10-18 09:11:14 +0200

    Authorization with middleware and adding members and dashboars pages

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,3 +7,13 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+// Creating express app and configuring middleware needed for authentication
+var app = express();
+
+app.use(express.static("public"));
+
+// Requiring our routes
+require("./html-routes.js")(app);//"./Routes/html-routes.js"
+
