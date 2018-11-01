7e9612dfe61bed3ff0b00f05a7919b6f2420f39d
rameshnagillaa <rameshnagillaa@gmail.com>
2018-11-01 20:42:40 +0530

    Admin Layout Components

---
+++ frontend/src/Components/StateFullComponents/AdminComponents/AdminHome/AdminLayout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { Container } from 'reactstrap';
+
+import { AppAside,
+  AppBreadcrumb,
+  AppFooter,
+  AppHeader,
+  AppSidebar,
+  AppSidebarFooter,
+  AppSidebarForm,
+  AppSidebarHeader,
+  AppSidebarMinimizer,
+  AppSidebarNav,
+} from '@coreui/react';
