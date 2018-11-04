838c3500b88a510506fc606f3b423bc771507f40
Techna987 <techna@windowslive.com>
2018-11-05 00:27:01 +0100

    Deployment

--- frontend/public/client.js
+++ frontend/public/client.js
@@ -1,68 +1,68 @@
-//const publicVapidKey =process.env.PUBLIC_VAPID_KEY;
+// const publicVapidKey =process.env.PUBLIC_VAPID_KEY;
 
-// import axios from "axios"
-// export default function subscribePush() {
-//   navigator.serviceWorker.ready.then(registration => {
-//     if (!registration.pushManager) {
-//       alert("Push Unsupported")
-//       return
-//     }
+// // import axios from "axios"
+// // export default function subscribePush() {
+// //   navigator.serviceWorker.ready.then(registration => {
+// //     if (!registration.pushManager) {
+// //       alert("Push Unsupported")
+// //       return
+// //     }
     
-//     registration.pushManager
-//       .subscribe({
-//         userVisibleOnly: true, //Always display notifications
-//         applicationServerKey: convertedVapidKey
-//       })
-//       .then(subscription => axios.post("/api/push/register", subscription))
-//       .catch(err => console.error("Push subscription error: ", err))
-//   })
+// //     registration.pushManager
+// //       .subscribe({
+// //         userVisibleOnly: true, //Always display notifications
+// //         applicationServerKey: convertedVapidKey
+// //       })
+// //       .then(subscription => axios.post("/api/push/register", subscription))
+// //       .catch(err => console.error("Push subscription error: ", err))
+// //   })
+// // }
+
+// // Check for service worker
+// if ("serviceWorker" in navigator) {
+//   send().catch(err => console.error(err));
 // }
 
-// Check for service worker
-if ("serviceWorker" in navigator) {
-  send().catch(err => console.error(err));
-}
+// // Register SW, Register Push, Send Push
+// async function send() {
+//   // Register Service Worker
+//   console.log("Registering service worker...");
+//   const register = await navigator.serviceWorker.register("..src/worker.js", {
+//     scope: "/"
+//   });
+//   console.log("Service Worker Registered...");
 
-// Register SW, Register Push, Send Push
-async function send() {
-  // Register Service Worker
-  console.log("Registering service worker...");
-  const register = await navigator.serviceWorker.register("..src/worker.js", {
-    scope: "/"
-  });
-  console.log("Service Worker Registered...");
+//   // Register Push
+//   console.log("Registering Push...");
+//   const subscription = await register.pushManager.subscribe({
+//     userVisibleOnly: true,//Always display notifications
+//     applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+//   });
+//   console.log("Push Registered...");
 
-  // Register Push
-  console.log("Registering Push...");
-  const subscription = await register.pushManager.subscribe({
-    userVisibleOnly: true //,Always display notifications
-    //applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
-  });
-  console.log("Push Registered...");
+//   // Send Push Notification
+//   console.log("Sending Push...");
+//   await fetch("/subscribe", {
+//     method: "POST",
+//     body: JSON.stringify(subscription),
+//     headers: {
+//       "content-type": "application/json"
+//     }
+//   });
+//   console.log("Push Sent...");
+// }
 
-  // Send Push Notification
-  console.log("Sending Push...");
-  await fetch("/subscribe", {
-    method: "POST",
-    body: JSON.stringify(subscription),
-    headers: {
-      "content-type": "application/json"
-    }
-  });
-  console.log("Push Sent...");
-}
+// function urlBase64ToUint8Array(base64String) {
+//   const padding = "=".repeat((4 - base64String.length % 4) % 4);
+//   const base64 = (base64String + padding)
+//     .replace(/\-/g, "+")
+//     .replace(/_/g, "/");
 
-function urlBase64ToUint8Array(base64String) {
-  const padding = "=".repeat((4 - base64String.length % 4) % 4);
-  const base64 = (base64String + padding)
-    .replace(/\-/g, "+")
-    .replace(/_/g, "/");
+//   const rawData = window.atob(base64);
+//   const outputArray = new Uint8Array(rawData.length);
 
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-}
+//   for (let i = 0; i < rawData.length; ++i) {
+//     outputArray[i] = rawData.charCodeAt(i);
+//   }
+//   return outputArray;
+// }
