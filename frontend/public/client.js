34851c0be1062e87709cdbc9e8330cfe7535623b
Techna987 <techna@windowslive.com>
2018-11-05 00:52:16 +0100

    deployment public keys

--- frontend/public/client.js
+++ frontend/public/client.js
@@ -1,8 +1,8 @@
 // const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
 // const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
 ///////// Public  Key ////////////////////////////////////////////////////////////////////
-//PRIVATE_VAPID_KEY="4IPU7p8KvW5ZUOflDb-TNZp8GyojX4O8Btn_thFk5X8"
-PUBLIC_VAPID_KEY= "BFSGOd7W_UObKJRIy0eXoqIKWkYpkM7imDBtE_Ds5aeE5f4LNw2h7yUQO9R5xQqDyfaNu_hf7kzzKhlCZrG_QZQ"
+//PRIVATE_VAPID_KEY="4IPU7p8KvW5ZUOflDb-TNZp8GyojX4O8Btn_thFk5X8";
+const PUBLIC_VAPID_KEY= "BFSGOd7W_UObKJRIy0eXoqIKWkYpkM7imDBtE_Ds5aeE5f4LNw2h7yUQO9R5xQqDyfaNu_hf7kzzKhlCZrG_QZQ";
 /////////////////////////////////////////////////////////////////////////////////////
 
 // import axios from "axios"
